c79265bd30cce8f25196485774f25ab145ca05df
Olivier-BeCode <76862461+Olivier-BeCode@users.noreply.github.com>
2021-01-30 14:30:09 +0100

    4-6

--- 01.Season-1/04-dates/04-locale-date/script.js
+++ 01.Season-1/04-dates/04-locale-date/script.js
@@ -7,5 +7,9 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let date = new Date();
+
+let options = { weekday: 'long', day: 'numeric', year: 'numeric', month: 'long' };
+document.getElementById("target").innerHTML = date.toLocaleDateString('en-GB', options) + ", " + date.getHours() + "h" + date.getMinutes();
 
 })();
--- 01.Season-1/04-dates/05-get-spooky-fridays/script.js
+++ 01.Season-1/04-dates/05-get-spooky-fridays/script.js
@@ -5,5 +5,25 @@
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+
+        let year = document.getElementById("year").value;
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+        let i = 0;
+        let months = "";
+
+            while (i < 12) {
+                var d = new Date(year, i, 13);
+                if (d.getDay() == 5) {
+                    months = months + " " + monthNames[d.getMonth()];
+                }
+                i++
+            }
+
+        alert(months);
+
+    
+    });
 
 })();
